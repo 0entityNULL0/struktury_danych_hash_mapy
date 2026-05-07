5d2dc2466fae4ac4dd9aa38422b6eddbde267f2a
0entityNULL0 <marcin_przed@wp.pl>
2026-05-07 22:36:40 +0200

    usprawnienie wektora aby byl zwiekszany/zmniejszamy

--- sources/hash_chain.h
+++ sources/hash_chain.h
@@ -21,27 +21,61 @@ private:
 	uint64_t ile_kubelkow;
 	uint64_t przesuniecie;
 	uint64_t mnoz;
-	std::vector<lista_key<T>*> *tablica;
+	std::vector<lista_key<T>*> tablica;
+	void zwieksz()
+	{
+		
+		std::vector<para<T>> *dane = all_data();
+		for(int i = 0;i<ile_kubelkow; i++)
+			delete tablica[i];
+		przechowywane_dane=0;
+		
+		ile_kubelkow*=2;
+		tablica.resize(ile_kubelkow);
+		for(int i = 0; i < ile_kubelkow;i++)
+		{
+			tablica[i]=new lista_key<T>;
+		}
+		for(int i = 0; i < dane->size();i++)
+			insert((*dane)[i].klucz,(*dane)[i].dane);
+		delete dane;
+	}
+	void zmniejsz()
+	{
+		std::vector<para<T>> *dane = all_data();
+		for(int i = 0;i<ile_kubelkow; i++)
+			delete tablica[i];
+		przechowywane_dane=0;
+		
+		ile_kubelkow/=2;
+		tablica.resize(ile_kubelkow);
+		for(int i = 0; i < ile_kubelkow;i++)
+		{
+			tablica[i]=new lista_key<T>;
+		}
+		for(int i = 0; i < dane->size();i++)
+			insert((*dane)[i].klucz,(*dane)[i].dane);
+		delete dane;
+		
+	}
 public:
 
 	hash_chain() 
 	{
 		przechowywane_dane=0;
-		tablica=new std::vector<lista_key<T>*>;
 		ile_kubelkow=3;
 		przesuniecie=0;
 		mnoz=1;
-		tablica->push_back(new lista_key<T>);
-		tablica->push_back(new lista_key<T>);
-		tablica->push_back(new lista_key<T>);
+		tablica.push_back(new lista_key<T>);
+		tablica.push_back(new lista_key<T>);
+		tablica.push_back(new lista_key<T>);
 	}
 	~hash_chain() 
 	{
 		for(int i = ile_kubelkow; i>0; i--)
 		{
-			delete (*tablica)[i-1];
+			delete tablica[i-1];
 		}
-		delete tablica;
 	}
 	
 	uint64_t size() override
@@ -55,8 +89,8 @@ public:
 		
 		for(int i = 0; i<ile_kubelkow; i++)
 		{
-			int rozmiar = (*tablica)[i]->size();;
-			para<T> *kubelek=(*tablica)[i]->zrob_tablice();
+			int rozmiar = tablica[i]->size();;
+			para<T> *kubelek=tablica[i]->zrob_tablice();
 			for(int j = 0; j<rozmiar; j++)
 			{
 				wynik->push_back(kubelek[j]);
@@ -71,8 +105,8 @@ public:
 		
 		for(int i = 0; i<ile_kubelkow; i++)
 		{
-			int rozmiar = (*tablica)[i-1]->size();
-			para<T> *kubelek=(*tablica)[i-1]->zrob_tablice();
+			int rozmiar = tablica[i-1]->size();
+			para<T> *kubelek=tablica[i-1]->zrob_tablice();
 			for(int j = 0; j<rozmiar; j++)
 			{
 				wynik->push_back(kubelek[j].klucz);
@@ -86,15 +120,18 @@ public:
 	{
 		
 		uint64_t hashowany_klucz=hash(string_for_hash(klucz),przesuniecie,ile_kubelkow,mnoz);
-		int a = (*tablica)[hashowany_klucz]->exterminate(klucz);
-		if(a==0)  przechowywane_dane--;
+		int a = tablica[hashowany_klucz]->exterminate(klucz);
+		if(a==0)  {
+			przechowywane_dane--;
+			if(przechowywane_dane<=ile_kubelkow) if(ile_kubelkow>3) zmniejsz();
+		}
 		return a;
 	}
 	int exists(std::string klucz)
 	{
 		
 		uint64_t hashowany_klucz=hash(string_for_hash(klucz),przesuniecie,ile_kubelkow,mnoz);
-		T* komorka = (*tablica)[hashowany_klucz]->search(klucz);
+		T* komorka = tablica[hashowany_klucz]->search(klucz);
 		if(komorka == nullptr) return 0;
 		return 1;
 	}
@@ -102,18 +139,21 @@ public:
 	void insert(std::string klucz, T dane) override
 	{
 		uint64_t hashowany_klucz=hash(string_for_hash(klucz),przesuniecie,ile_kubelkow,mnoz);
+		//T* komorka = tablica[hashowany_klucz]->search(klucz);
+		//if(T!=nullptr) throw "ten klucz juz istnieje";
 		para<T> par;
 		par.klucz=klucz;
 		par.dane=dane;
-		(*tablica)[hashowany_klucz]->add(par);
+		tablica[hashowany_klucz]->add(par);
 		przechowywane_dane++;
+		if(przechowywane_dane/ile_kubelkow>=3) zwieksz();
 	}
 	
 	
     T & operator [](std::string klucz) override
     {
 		uint64_t hashowany_klucz=hash(string_for_hash(klucz),przesuniecie,ile_kubelkow,mnoz);
-		T* komorka = (*tablica)[hashowany_klucz]->search(klucz);
+		T* komorka = tablica[hashowany_klucz]->search(klucz);
 		if(komorka == nullptr) throw "nie istnieje w tablicy taki klucz";
 		return *komorka;
 		
@@ -121,7 +161,7 @@ public:
 	T operator [] (std::string klucz) const override
 	{
 		uint64_t hashowany_klucz=hash(string_for_hash(klucz),przesuniecie,ile_kubelkow,mnoz);
-		T* komorka = (*tablica)[hashowany_klucz]->search(klucz);
+		T* komorka = tablica[hashowany_klucz]->search(klucz);
 		if(komorka == nullptr) throw "nie istnieje w tablicy taki klucz";
 		return *komorka;
 	}
--- unit_testy.cpp
+++ unit_testy.cpp
@@ -7,7 +7,7 @@
 template <template  <typename> typename TMAP> int testuj_hashmap()
 {
 	TMAP <int> hmap;
-	std::string testowe_klucze[5]={"jeden","dwa","trzy","cztery","piec"};
+	std::string testowe_klucze[10]={"jeden","dwa","trzy","cztery","piec","szesc","siedem","osiem","dziewiec","zero"};
 	if(hmap.size()!=0) return 1;
 	std::vector <para<int>> *zawartosc;
 	for(int i = 0; i < 5; i++)
@@ -34,13 +34,13 @@ template <template  <typename> typename TMAP> int testuj_hashmap()
 	for(int i = 0; i < 5; i++) if(ile[i]!=1) return 5+i*10;
 	for(int i = 0; i < 5; i++)
 	{
-		if(hmap[testowe_klucze[4-i]]!=4-i) //do zmiany
+		if(hmap[testowe_klucze[4-i]]!=4-i) 
 			return 6+i*10;
 	}
 	for(int i = 0; i < 5; i++)
 	{
 		hmap[testowe_klucze[i]]=i+5;
-		if(hmap[testowe_klucze[i]]!=i+5) //do zmiany
+		if(hmap[testowe_klucze[i]]!=i+5) 
 			return 7+i*10;
 	}
 	
@@ -48,11 +48,24 @@ template <template  <typename> typename TMAP> int testuj_hashmap()
 	{
 		hmap.remove(testowe_klucze[i]);
 		for(int j = 0; j <i; j++)
-			if(hmap[testowe_klucze[j]]!=j+5) //do zmiany
+			if(hmap[testowe_klucze[j]]!=j+5) 
 				return 8+i*10;
 		if(hmap.exists(testowe_klucze[i])==1)
 			return 8+i*10;
 	}
+	
+	for(int i = 0; i < 10; i++)
+	{
+		
+		hmap.insert(testowe_klucze[i],i);
+	}
+	zawartosc=hmap.all_data();
+	for(int i = 0; i < zawartosc->size(); i++)
+		std::cout<<(*zawartosc)[i].klucz<<'|'<<(*zawartosc)[i].dane<<'\n';
+	delete zawartosc;
+		
+	for(int i = 0; i < 10; i++)
+		hmap.remove(testowe_klucze[i]);
 	return 0;
 }
 
